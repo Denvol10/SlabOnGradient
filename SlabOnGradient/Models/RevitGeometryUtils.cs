e3d91e434001ece5ed2146b4e881fb0829f1b407
Денис Щербатюк <denvol100@gmail.com>
2023-10-11 15:28:41 +0300

    Add feat select road axis and road lines by user

---
+++ SlabOnGradient/Models/RevitGeometryUtils.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using SlabOnGradient.Models.Filters;
+
+namespace SlabOnGradient.Models
+{
+    internal class RevitGeometryUtils
+    {
+        public static List<Curve> GetCurvesByRectangle(UIApplication uiapp, out string elementIds)
+        {
+            Selection sel = uiapp.ActiveUIDocument.Selection;
+            var selectedElements = sel.PickElementsByRectangle(new DirectShapeClassFilter() ,"Select Road Axis");
+            var directshapeRoadAxis = selectedElements.OfType<DirectShape>();
+            elementIds = ElementIdToString(directshapeRoadAxis);
+            var curvesRoadAxis = GetCurvesByDirectShapes(directshapeRoadAxis);
+
+            return curvesRoadAxis;
+        }
+
+        // Метод получения списка линий на поверхности дороги
+        public static List<Line> GetRoadLines(UIApplication uiapp, out string elementIds)
+        {
+            Selection sel = uiapp.ActiveUIDocument.Selection;
+            var selectedOnRoadSurface = sel.PickObjects(ObjectType.Element, "Select Road Lines");
+            var directShapesRoadSurface = selectedOnRoadSurface.Select(r => uiapp.ActiveUIDocument.Document.GetElement(r))
+                                                               .OfType<DirectShape>();
+            elementIds = ElementIdToString(directShapesRoadSurface);
+            var curvesRoadSurface = GetCurvesByDirectShapes(directShapesRoadSurface);
+            var linesRoadSurface = curvesRoadSurface.OfType<Line>().ToList();
+
+            return linesRoadSurface;
+        }
+
+        // Получение линий границ плиты
+        public static Curve GetBoundCurve(UIApplication uiapp, out string elementIds)
+        {
+            Selection sel = uiapp.ActiveUIDocument.Selection;
+            var boundCurvePicked = sel.PickObject(ObjectType.Element, "Выберете линию границы плиты");
+            Options options = new Options();
+            Element curveElement = uiapp.ActiveUIDocument.Document.GetElement(boundCurvePicked);
+            elementIds = "Id" + curveElement.Id.IntegerValue;
+            var boundCurve = curveElement.get_Geometry(options).First() as Curve;
+
+            return boundCurve;
+        }
+
+        // Получение линии из списка, которая пересекается с плоскостью
+        public static Line GetIntersectCurve(IEnumerable<Line> lines, Plane plane)
+        {
+            XYZ originPlane = plane.Origin;
+            XYZ directionLine = plane.XVec;
+
+            var lineByPlane = Line.CreateUnbound(originPlane, directionLine);
+
+            foreach (var line in lines)
+            {
+                XYZ startPoint = line.GetEndPoint(0);
+                XYZ finishPoint = line.GetEndPoint(1);
+
+                XYZ startPointOnBase = new XYZ(startPoint.X, startPoint.Y, 0);
+                XYZ finishPointOnBase = new XYZ(finishPoint.X, finishPoint.Y, 0);
+
+                var baseLine = Line.CreateBound(startPointOnBase, finishPointOnBase);
+
+                var result = new IntersectionResultArray();
+                var compResult = lineByPlane.Intersect(baseLine, out result);
+                if (compResult == SetComparisonResult.Overlap)
+                {
+                    return line;
+                }
+            }
+
+            return null;
+        }
+
+        /* Пересечение линии и плоскости
+         * (преобразует линию в вектор, поэтому пересекает любую линию не параллельную плоскости)
+         */
+        public static XYZ LinePlaneIntersection(Line line, Plane plane, out double lineParameter)
+        {
+            XYZ planePoint = plane.Origin;
+            XYZ planeNormal = plane.Normal;
+            XYZ linePoint = line.GetEndPoint(0);
+
+            XYZ lineDirection = (line.GetEndPoint(1) - linePoint).Normalize();
+
+            // Проверка на параллельность линии и плоскости
+            if ((planeNormal.DotProduct(lineDirection)) == 0)
+            {
+                lineParameter = double.NaN;
+                return null;
+            }
+
+            lineParameter = (planeNormal.DotProduct(planePoint)
+              - planeNormal.DotProduct(linePoint))
+                / planeNormal.DotProduct(lineDirection);
+
+            return linePoint + lineParameter * lineDirection;
+        }
+
+
+        // Получение id элементов на основе списка в виде строки
+        public static List<int> GetIdsByString(string elems)
+        {
+            if (string.IsNullOrEmpty(elems))
+            {
+                return null;
+            }
+
+            var elemIds = elems.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                         .Select(s => int.Parse(s.Remove(0, 2)))
+                         .ToList();
+
+            return elemIds;
+        }
+
+        // Проверка на то существуют ли элементы с данным Id в модели
+        public static bool IsElemsExistInModel(Document doc, IEnumerable<int> elems, Type type)
+        {
+            if (elems is null)
+            {
+                return false;
+            }
+
+            foreach (var elem in elems)
+            {
+                ElementId id = new ElementId(elem);
+                Element curElem = doc.GetElement(id);
+                if (curElem is null || !(curElem.GetType() == type))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Получение линий по их id
+        public static List<Curve> GetCurvesById(Document doc, IEnumerable<int> ids)
+        {
+            var directShapeLines = new List<DirectShape>();
+            foreach (var id in ids)
+            {
+                ElementId elemId = new ElementId(id);
+                DirectShape line = doc.GetElement(elemId) as DirectShape;
+                directShapeLines.Add(line);
+            }
+
+            var lines = GetCurvesByDirectShapes(directShapeLines).OfType<Curve>().ToList();
+
+            return lines;
+        }
+
+        // Получение линии по Id
+        public static Curve GetBoundCurveById(Document doc, string elemIdInSettings)
+        {
+            var elemId = GetIdsByString(elemIdInSettings).First();
+            ElementId modelLineId = new ElementId(elemId);
+            Element modelLine = doc.GetElement(modelLineId);
+            Options options = new Options();
+            Curve line = modelLine.get_Geometry(options).First() as Curve;
+
+            return line;
+        }
+
+        // Получение линий на основе элементов DirectShape
+        private static List<Curve> GetCurvesByDirectShapes(IEnumerable<DirectShape> directShapes)
+        {
+            var curves = new List<Curve>();
+
+            Options options = new Options();
+            var geometries = directShapes.Select(d => d.get_Geometry(options)).SelectMany(g => g);
+
+            foreach (var geom in geometries)
+            {
+                if (geom is PolyLine polyLine)
+                {
+                    var polyCurve = GetCurvesByPolyline(polyLine);
+                    curves.AddRange(polyCurve);
+                }
+                else
+                {
+                    curves.Add(geom as Curve);
+                }
+            }
+
+            return curves;
+        }
+
+        // Метод получения списка линий на основе полилинии
+        private static IEnumerable<Curve> GetCurvesByPolyline(PolyLine polyLine)
+        {
+            var curves = new List<Curve>();
+
+            for (int i = 0; i < polyLine.NumberOfCoordinates - 1; i++)
+            {
+                var line = Line.CreateBound(polyLine.GetCoordinate(i), polyLine.GetCoordinate(i + 1));
+                curves.Add(line);
+            }
+
+            return curves;
+        }
+
+        // Метод получения строки с ElementId
+        private static string ElementIdToString(IEnumerable<Element> elements)
+        {
+            var stringArr = elements.Select(e => "Id" + e.Id.IntegerValue.ToString()).ToArray();
+            string resultString = string.Join(", ", stringArr);
+
+            return resultString;
+        }
+    }
+}
--- SlabOnGradient/Models/RevitModelForfard.cs
+++ SlabOnGradient/Models/RevitModelForfard.cs
@@ -10,6 +10,7 @@ using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.DB.Architecture;
 using System.Collections.ObjectModel;
+using SlabOnGradient.Models;
 
 namespace SlabOnGradient
 {
@@ -27,6 +28,99 @@ namespace SlabOnGradient
             Uidoc = uiapp.ActiveUIDocument;
             Doc = uiapp.ActiveUIDocument.Document;
         }
+
+        #region Ось трассы
+        public PolyCurve RoadAxis { get; set; }
+
+        private string _roadAxisElemIds;
+        public string RoadAxisElemIds
+        {
+            get => _roadAxisElemIds;
+            set => _roadAxisElemIds = value;
+        }
+
+        public void GetPolyCurve()
+        {
+            var curves = RevitGeometryUtils.GetCurvesByRectangle(Uiapp, out _roadAxisElemIds);
+            RoadAxis = new PolyCurve(curves);
+        }
+        #endregion
+
+        #region Проверка на то существуют линии оси и линии на поверхности в модели
+        public bool IsLinesExistInModel(string elemIdsInSettings)
+        {
+            var elemIds = RevitGeometryUtils.GetIdsByString(elemIdsInSettings);
+
+            return RevitGeometryUtils.IsElemsExistInModel(Doc, elemIds, typeof(DirectShape));
+        }
+        #endregion
+
+        #region Проверка на то существует линия границы плиты
+        public bool IsBoundLineExistInModel(string elemIdsInSettings)
+        {
+            var elemIds = RevitGeometryUtils.GetIdsByString(elemIdsInSettings);
+
+            return RevitGeometryUtils.IsElemsExistInModel(Doc, elemIds, typeof(ModelLine));
+        }
+        #endregion
+
+        #region Получение оси трассы из Settings
+        public void GetAxisBySettings(string elemIdsInSettings)
+        {
+            var elemIds = RevitGeometryUtils.GetIdsByString(elemIdsInSettings);
+            var lines = RevitGeometryUtils.GetCurvesById(Doc, elemIds);
+            RoadAxis = new PolyCurve(lines);
+        }
+        #endregion
+
+        #region Линия на поверхности 1
+        public List<Line> RoadLines1 { get; set; }
+
+        private string _roadLineElemIds1;
+        public string RoadLineElemIds1
+        {
+            get => _roadLineElemIds1;
+            set => _roadLineElemIds1 = value;
+        }
+
+        public void GetRoadLine1()
+        {
+            RoadLines1 = RevitGeometryUtils.GetRoadLines(Uiapp, out _roadLineElemIds1);
+        }
+        #endregion
+
+        #region Получение линии на поверхности 1 из Settings
+        public void GetRoadLines1BySettings(string elemIdsInSettings)
+        {
+            var elemIds = RevitGeometryUtils.GetIdsByString(elemIdsInSettings);
+            RoadLines1 = RevitGeometryUtils.GetCurvesById(Doc, elemIds).OfType<Line>().ToList();
+        }
+        #endregion
+
+        #region Линия на поверхности 2
+        public List<Line> RoadLines2 { get; set; }
+
+        private string _roadLineElemIds2;
+        public string RoadLineElemIds2
+        {
+            get => _roadLineElemIds2;
+            set => _roadLineElemIds2 = value;
+        }
+
+        public void GetRoadLine2()
+        {
+            RoadLines2 = RevitGeometryUtils.GetRoadLines(Uiapp, out _roadLineElemIds2);
+        }
+        #endregion
+
+        #region Получение линии на поверхности 2 из Settings
+        public void GetRoadLines2BySettings(string elemIdsInSettings)
+        {
+            var elemIds = RevitGeometryUtils.GetIdsByString(elemIdsInSettings);
+            RoadLines2 = RevitGeometryUtils.GetCurvesById(Doc, elemIds).OfType<Line>().ToList();
+        }
+        #endregion
+
 
     }
 }
